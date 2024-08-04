dde7b4e24f3964cb2e472e31eb6523d9941258e6
Ahmer-Rafique <ahmerali1771@gmail.com>
2024-08-04 16:53:27 +0500

    add class work

---
+++ Python/class 2/index.py
@@ -0,0 +1,11 @@
+name:str="Ahmer Ali";
+age:int=18;
+hieght:float=5.3
+hobbies:list=["Cricket","Programming","coding"]
+Male:bool=True
+print(name)
+print(age)
+print(hieght)
+print(hobbies)
+print(Male)
+# in class 2 we read about variables and these types of variable in python
